dc7355dba5b56028a4a9a33cbc4b187f7affdcc3
Philipp Dikmann <mail@philippdikmann.de>
2021-06-02 13:07:31 +0200

    add react-draggable, add handler to crop

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -3,25 +3,14 @@ import '../CSS/App.css';
 import image from '../andriyko-podilnyk-RCfi7vgJjUY-unsplash.jpg'
 import {Wrapper} from "./Wrapper";
 import {ImageDisplay} from "./ImageDisplay";
-import classes from '../CSS/Crop.module.css'
-
-interface ICropProps {
-}
-
-function Crop(){
-  return (
-    <div className={classes.crop}>
-      <div className={classes.handle}/>
-    </div>
-  )
-}
+import {Crop} from "./Crop";
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
         <Crop/>
-        <ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'}/>
+        {/*<ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'}/>*/}
       </Wrapper>
     </div>
   );
