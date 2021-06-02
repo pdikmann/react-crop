15e161e87df0ff99a3c42d779934466fe5b83389
Philipp Dikmann <mail@philippdikmann.de>
2021-06-02 11:26:35 +0200

    add ImageDisplay, add image file.

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import './App.css';
+import '../CSS/App.css';
+import image from '../andriyko-podilnyk-RCfi7vgJjUY-unsplash.jpg'
+
+interface IImageDisplay {
+  src: string
+  alt: string
+}
+
+function ImageDisplay (props: IImageDisplay){
+  return <img src={props.src} alt={props.alt} />
+}
 
 function App() {
   return (
     <div className="App">
+      <ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'} />
     </div>
   );
 }
