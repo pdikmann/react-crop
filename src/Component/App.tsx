d6409a4958cf37c543860fad5c4a82876fa41ad2
Philipp Dikmann <mail@philippdikmann.de>
2021-06-02 14:39:16 +0200

    setting css properties

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,19 +1,18 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
 import '../CSS/App.css';
 import image from '../andriyko-podilnyk-RCfi7vgJjUY-unsplash.jpg'
 import {CenteringWrapper} from "./CenteringWrapper";
 import {ImageDisplay} from "./ImageDisplay";
-import {Crop} from "./Crop";
-import {SizeWrapper} from "./SizeWrapper";
+import {Crop} from "./Crop"
+import {SizeWrapper} from "./SizeWrapper"
 
 function App() {
-  let imgCmpRef = React.createRef<ReactNode>()
   return (
     <div className="App">
       <CenteringWrapper>
-        <SizeWrapper width={'800px'} height={'600px'}>
+        <SizeWrapper width={'400px'} height={'300px'}>
           <ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'}/>
-          <Crop/>
+          <Crop/> {/*TODO envelop image*/}
         </SizeWrapper>
       </CenteringWrapper>
     </div>
--- src/Component/Crop.tsx
+++ src/Component/Crop.tsx
@@ -1,37 +1,95 @@
 import classes from "../CSS/Crop.module.css";
 import React from "react";
-import Draggable, {DraggableEventHandler} from 'react-draggable'
+import Draggable, {DraggableData, DraggableEvent, DraggableEventHandler} from 'react-draggable'
 
 interface ICropProps {
 }
 
 interface IHandleProps {
   dragHandler: DraggableEventHandler
+  role: HandleRoles
 }
 
 function Handle(props: IHandleProps) {
   let ref = React.createRef<HTMLDivElement>()
+  let label = ['tl', 'tr', 'br', 'bl'][props.role as number]
   return (
     <Draggable nodeRef={ref} onDrag={props.dragHandler}>
-      <div ref={ref} className={classes.handle}/>
+      <div ref={ref} className={classes.handle}>{label}</div>
     </Draggable>
   )
 }
 
+enum HandleRoles {
+  TopLeft,
+  TopRight,
+  BottomRight,
+  BottomLeft,
+  MAX
+}
+
+enum OverlayRoles {
+  Top,
+  Right,
+  Bottom,
+  Left,
+  MAX
+}
+
+type Test = 'a'
+
+
 export class Crop extends React.Component<ICropProps> {
-  constructor(props: ICropProps) {
-    super(props);
-    this.handleDrag = this.handleDrag.bind(this)
+  // overlays: {
+  //   [OverlayRoles]: React.Ref<HTMLDivElement>
+  // }
+  overlayRoles = [OverlayRoles.Left]
+  cropFrame = React.createRef<HTMLDivElement>()
+  handleRoles = [HandleRoles.TopLeft, HandleRoles.BottomRight] //[HandleRoles.TopLeft, HandleRoles.TopRight, HandleRoles.BottomRight, HandleRoles.BottomLeft]
+  cropFrameDimensions : {
+    top: number
+    left: number
+    width: number
+    height: number
   }
 
-  handleDrag() {
+  constructor(props: ICropProps) {
+    super(props);
+    this.cropFrameDimensions = {top: 0, left: 0, width: 0, height: 0}
+    // this.overlays = this.overlayRoles.map((role) => ({role, ref: React.createRef<HTMLDivElement>()}))
+    this.createDragHandler = this.createDragHandler.bind(this)
+  }
+
+  createDragHandler(role: HandleRoles) {
+    let cropFrame = this.cropFrame
+    return (e: DraggableEvent, data: DraggableData) => {
+      let cropFrame = this.cropFrame.current as HTMLDivElement;
+      switch (role) {
+        case HandleRoles.TopLeft:
+          cropFrame.style.setProperty('left', `${data.x}px`)
+          cropFrame.style.setProperty('top', `${data.y}px`)
+          break
+        case HandleRoles.BottomRight:
+
+          break
+      }
+    }
+  }
+
+  dragHandler(e: DraggableEvent, data: DraggableData): void {
     console.log('dragged')
   }
 
   render() {
     return (
       <div className={classes.crop}>
-        <Handle dragHandler={this.handleDrag}/>
+        <div ref={this.cropFrame} className={classes.cropFrame} style={{top: '100px'}}/>
+        {/*{this.overlays.map(({role, ref}) => (*/}
+        {/*  <div ref={ref} className={classes.overlay}/>*/}
+        {/*))}*/}
+        {this.handleRoles.map((role: HandleRoles) => (
+          <Handle role={role} dragHandler={this.createDragHandler(role)}/>
+        ))}
       </div>
     )
   }
