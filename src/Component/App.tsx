c29449261953ccd4f64f8258c680457e87fe1057
Philipp Dikmann <mail@philippdikmann.de>
2021-06-02 11:42:51 +0200

    set CSS variable in ImageDisplay.

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -3,21 +3,31 @@ import '../CSS/App.css';
 import image from '../andriyko-podilnyk-RCfi7vgJjUY-unsplash.jpg'
 import '../CSS/ImageDisplay.css'
 
-interface IImageDisplay {
+interface IImageDisplayProps {
   src: string
   alt: string
-  width?: number
-  height?: number
+  width?: string
+  height?: string
 }
 
-function ImageDisplay (props: IImageDisplay){
-  return <img className="image-display" src={props.src} alt={props.alt} />
+class ImageDisplay extends React.Component<IImageDisplayProps> {
+  imgRef = React.createRef<HTMLImageElement>()
+
+  componentDidMount() {
+    let elm = this.imgRef.current as HTMLImageElement
+    elm.style.setProperty('--custom-width', this.props.width || '50%')
+    elm.style.setProperty('--custom-height', this.props.height || '50%')
+  }
+
+  render() {
+    return <img ref={this.imgRef} className="image-display" src={this.props.src} alt={this.props.alt}/>
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'} />
+      <ImageDisplay src={image} alt={'Photo by Andriyko Podilnyk (https://unsplash.com/@yirage)'}/>
     </div>
   );
 }
