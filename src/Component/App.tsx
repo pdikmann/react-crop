2950e26c73c65eaba36260e4efd9c642de7cef29
Philipp Dikmann <mail@philippdikmann.de>
2021-06-02 11:44:23 +0200

    move ImageDisplay into own file.

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
 import '../CSS/App.css';
 import image from '../andriyko-podilnyk-RCfi7vgJjUY-unsplash.jpg'
-import '../CSS/ImageDisplay.css'
-
-interface IImageDisplayProps {
-  src: string
-  alt: string
-  width?: string
-  height?: string
-}
-
-class ImageDisplay extends React.Component<IImageDisplayProps> {
-  imgRef = React.createRef<HTMLImageElement>()
-
-  componentDidMount() {
-    let elm = this.imgRef.current as HTMLImageElement
-    elm.style.setProperty('--custom-width', this.props.width || '50%')
-    elm.style.setProperty('--custom-height', this.props.height || '50%')
-  }
-
-  render() {
-    return <img ref={this.imgRef} className="image-display" src={this.props.src} alt={this.props.alt}/>
-  }
-}
+import {ImageDisplay} from "./ImageDisplay";
 
 function App() {
   return (
---
+++ src/Component/ImageDisplay.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../CSS/ImageDisplay.css'
+
+interface IImageDisplayProps {
+  src: string
+  alt: string
+  width?: string
+  height?: string
+}
+
+export class ImageDisplay extends React.Component<IImageDisplayProps> {
+  imgRef = React.createRef<HTMLImageElement>()
+
+  componentDidMount() {
+    let elm = this.imgRef.current as HTMLImageElement
+    elm.style.setProperty('--custom-width', this.props.width || '50%')
+    elm.style.setProperty('--custom-height', this.props.height || '50%')
+  }
+
+  render() {
+    return <img ref={this.imgRef} className="image-display" src={this.props.src} alt={this.props.alt}/>
+  }
+}
